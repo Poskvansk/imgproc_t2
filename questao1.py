a7a4e7dc02ad3ce2586542997d34aeedbaf4f101
Poskvansk <luisfilipesrct@gmail.com>
2021-04-28 23:41:10 -0300

    Questao 2 morfologia quase pronta, ajustes faltam

--- questao1.py
+++ questao1.py
@@ -14,103 +14,52 @@ def show(img):
 
 def binarization(img):
 
-    kernel = np.ones((5,5),np.uint8)
+    # kernel = np.ones((5,5),np.uint8)
 
-    ret, binary_image = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    ret, binary_image = cv2.threshold(img, 20, 255, cv2.THRESH_BINARY)
 
-    binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel)
+    # binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel)
 
     return binary_image
 
-def is_empty(img):
-    
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def morph_operations(img):
 
-    h = img.shape[0]
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (100,100))
+    # kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (50,50))
 
-    for i in range(h):
-        if (np.sum(img[i]) != 0):
-            return False
+     
+    # closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    # show(closed)
+    opened = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    # show(opened)
+    # smooth = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
+    # show(smooth)
 
-    return True
+    tophat = img - opened
+    # show(tophat)
 
-def get_diameter(img):
+    # bottomhat = closed - img
+    # show(bottomhat)
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    h = img.shape[0]
+    return tophat
 
-    arr = np.array([], dtype=np.int32)
-
-    for i in range(h):
-        if (np.sum(img[i]) != 0):
-            arr = np.append(arr, np.sum(img[i]))
-
-    diameter = int((max(arr)/255)/2) -1
-
-    return diameter
-
-def eliminate_cookie(img, diameter):
-
-    #eliminar a cookie mordida
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (diameter,diameter))
-
-    erode = cv2.morphologyEx(img, cv2.MORPH_ERODE, kernel)
-
-    return erode
-
-def erode_image(img):
-
-    diameter = get_diameter(img)
-
-    erode = eliminate_cookie(img, diameter)
-
-    while(is_empty(erode)):
-        diameter -= 1
-        erode = eliminate_cookie(img, diameter)
-
-    return erode
-
-def recuperar(original_img, new_img):
-
-    #recuperar forma inicial da cookie na imagem binarizada
-
-    diameter = get_diameter(original_img)
-    
-    diameter += int(0.15 * diameter)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (diameter,diameter))
-
-    recovered_cookie = cv2.morphologyEx(new_img, cv2.MORPH_DILATE, kernel)
- 
-    return recovered_cookie
-
-def alterar_imagem(img, mask):
-
-    # usar imagem da funcao recupear como mascara para deixar so a cookie completa na imagem original
-
-    result = cv2.bitwise_and(img, mask)
-
-    return result
 
 
 def main():
 
-    img = cv2.imread("imagens/cookies.tif")
+    img = cv2.imread("imagens/morf_test.png")
     # show(img)
 
-    binary_image = binarization(img)
-    # show(binary_image)
+    # binary_img = binarization(img)
+    # # show(binary_img)
 
-    erode = erode_image(binary_image)
-    # show(erode)
+    test = morph_operations(img)
+    # show(test)
 
-    recovered_cookie_mask = recuperar(binary_image, erode)
-    # show(recovered_cookie_mask)
-
-    result = alterar_imagem(img, recovered_cookie_mask)
-    show(result)
-    #test
-
+    binary_img = binarization(test)
+    show(binary_img)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_ERODE, kernel)
+    show(binary_img)
 
 main()
--- questao2.py
+++ questao2.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def show(img):
 
     cv2.imshow("image", img)
@@ -11,34 +12,105 @@ def show(img):
     # plt.imshow(img[:,:,::-1]); plt.show()
     # plt.imshow(img, cmap='gray', vmin=0, vmax=255); plt.show()
 
-
-def morfologico(img):
-
-    #CRIAR OPERACAO MORFOLOGICA PRA MELHORAR BINARIZACAO DA IMAGEM
-
-    return
-
 def binarization(img):
 
-    ret,thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    show(thresh)
+    kernel = np.ones((5,5),np.uint8)
 
-    # img = cv2.medianBlur(img, 5)
+    ret, binary_image = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+    binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel)
+
+    return binary_image
+
+def is_empty(img):
     
-    # # th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)    
-    # # show(th2)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    # show(th2)
+    h = img.shape[0]
 
-    # th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    # show(th3)
+    for i in range(h):
+        if (np.sum(img[i]) != 0):
+            return False
+
+    return True
+
+def get_diameter(img):
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    h = img.shape[0]
+
+    arr = np.array([], dtype=np.int32)
+
+    for i in range(h):
+        if (np.sum(img[i]) != 0):
+            arr = np.append(arr, np.sum(img[i]))
+
+    diameter = int((max(arr)/255)/2) -1
+
+    return diameter
+
+def eliminate_cookie(img, diameter):
+
+    #eliminar a cookie mordida
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (diameter,diameter))
+
+    erode = cv2.morphologyEx(img, cv2.MORPH_ERODE, kernel)
+
+    return erode
+
+def erode_image(img):
+
+    diameter = get_diameter(img)
+
+    erode = eliminate_cookie(img, diameter)
+
+    while(is_empty(erode)):
+        diameter -= 1
+        erode = eliminate_cookie(img, diameter)
+
+    return erode
+
+def recuperar(original_img, new_img):
+
+    #recuperar forma inicial da cookie na imagem binarizada
+
+    diameter = get_diameter(original_img)
+    
+    diameter += int(0.15 * diameter)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (diameter,diameter))
+
+    recovered_cookie = cv2.morphologyEx(new_img, cv2.MORPH_DILATE, kernel)
+ 
+    return recovered_cookie
+
+def alterar_imagem(img, mask):
+
+    # usar imagem da funcao recupear como mascara para deixar so a cookie completa na imagem original
+
+    result = cv2.bitwise_and(img, mask)
+
+    return result
 
 
 def main():
 
-    img = cv2.imread("imagens/morf_test.png")
+    img = cv2.imread("imagens/cookies.tif")
     # show(img)
-    binarization(img)
+
+    binary_image = binarization(img)
+    # show(binary_image)
+
+    erode = erode_image(binary_image)
+    # show(erode)
+
+    recovered_cookie_mask = recuperar(binary_image, erode)
+    # show(recovered_cookie_mask)
+
+    result = alterar_imagem(img, recovered_cookie_mask)
+    show(result)
+    #test
+
 
 main()
